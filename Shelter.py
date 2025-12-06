50835f354597d3a1ed9f50444f1f73d9ab956dbe
noeshoor <n_shoor@coloradocollege.edu>
2025-12-06 15:48:01 -0700

    added other sttributes to shelter class

--- Shelter.py
+++ Shelter.py
@@ -1,10 +1,16 @@
 class Shelter:
 
-    def __init__(self, title, bio, numTotBeds, numOpenBeds):
+    def __init__(self, title, bio, numTotBeds, numOpenBeds, womenOnly, youth, emergency, family, latitude, longitude):
         self.title = title
         self.bio = bio
         self.numTotBeds = numTotBeds
         self.numOpenBeds = numOpenBeds
+        self.womenOnly = womenOnly
+        self.youth = youth
+        self.emergency = emergency
+        self.family = family
+        self.latitude = latitude
+        self.longitude
 
     def setTitle(self, title):
         self.title = title
@@ -35,7 +41,42 @@ class Shelter:
 
     def removeOpenBeds(self, numBeds):
         self.numOpenBeds -= numBeds
+
+    def getWomenOnly(self):
+        return self.womenOnly
+
+    def setWomenOnly(self, womenOnly):
+         self.womenOnly = womenOnly
+
+    def getYouth(self):
+        return self.youth
+
+    def setYouth(self, youth):
+        self.youth = youth
+
+    def getEmergency(self):
+        return self.emergency
+
+    def setEmergency(self, emergency):
+        self.emergency = emergency
+
+    def getFamily(self):
+        return self.family
+
+    def setFamily(self, family):
+        self.family = family
+
+    def getLatitude(self):
+        return self.latitude
+
+    def setLatitude(self, latitude):
+        self.latitude = latitude
     
+    def getLongitude(self):
+        return self.longitude
+
+    def setLongitude(self, longitude):
+        self.longitude = longitude
 
 
 
